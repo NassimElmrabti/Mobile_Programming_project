51e585040135552d61e4b41609880974a1c4f860
kalash94 <elmrabti@et.esiea.fr>
2020-05-06 11:15:11 +0200

    Ajout de l'appel serveur

--- app/src/main/java/com/example/td3/MainActivity.java
+++ app/src/main/java/com/example/td3/MainActivity.java
@@ -5,11 +5,23 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    static final String BASE_URL = "https://raw.githubusercontent.com/kalash94/Mobile_Programming_project/API_Rest/app/src/main/java/com/example/td3/";
 
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
@@ -20,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       showList();
+       makeApiCall();
+    }
+
+    private void showList() {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         // use this setting to
         // improve performance if you know that changes
@@ -35,5 +52,42 @@ public class MainActivity extends AppCompatActivity {
         }// define an adapter
         mAdapter = new ListAdapter(input);
         recyclerView.setAdapter(mAdapter);
+    }
+
+    private void makeApiCall(){
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        FinalFantasyApi FinalFantasyApi = retrofit.create(FinalFantasyApi.class);
+
+        Call<RestFinalFantasyResponse> call = FinalFantasyApi.getFinalFantasyResponse();
+        call.enqueue(new Callback<RestFinalFantasyResponse>() {
+            @Override
+            public void onResponse(Call<RestFinalFantasyResponse> call, Response<RestFinalFantasyResponse> response) {
+                if (response.isSuccessful() && response.body() != null){
+                    List<FinalFantasy> FinalFantasyList = response.body().getResults();
+                    Toast.makeText(getApplicationContext(), "API Success", Toast.LENGTH_SHORT).show();
+                }else{
+                    showError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RestFinalFantasyResponse> call, Throwable t) {
+                showError();
+            }
+        });
+
+
+    }
+
+    private void showError() {
+        Toast.makeText(getApplicationContext(), "API Error", Toast.LENGTH_SHORT).show();
     }
 }
