b86931336f9b7aa8e690545682b7fa2cc7d3b7d5
kalash94 <elmrabti@et.esiea.fr>
2020-06-21 21:18:22 +0200

    Home screen added

--- app/src/main/java/com/example/td3/Constants.java
+++ app/src/main/java/com/example/td3/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String BASE_URL = "https://raw.githubusercontent.com/kalash94/Mobile_Programming_project/master/app/src/main/java/com/example/td3/";
     public static final String KEY_FINAL_FANTASY_INTENT = "FinalFantasyKey";
     public static String KEY_FINAL_FANTASY_LIST = "jsonFinalFantasyList";
+    public static int SPLASH_SCREEN = 6000;
 }
---
+++ app/src/main/java/com/example/td3/presentation/view/HomeActivity.java
@@ -0,0 +1,50 @@
+package com.example.td3.presentation.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.td3.Constants;
+import com.example.td3.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_home);
+
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, Constants.SPLASH_SCREEN);
+    }
+}
